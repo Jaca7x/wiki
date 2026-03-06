ed469f29b87c4e3d7033b9bb3514d563d77fe5a4
Jaca <eduardo.costa.miguel@escola.pr.gov.br>
2026-03-05 21:35:11 -0300

    frat: Reformulando código do home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
-import logo from "../assets/icon.png";
-import background from "../assets/background-home.png";
-import initialTitle from "../assets/initial-title.png";
+import logo from "../assets/imgs/icon/icon.png";
+import background from "../assets/imgs/home/background-home.png";
+import initialTitle from "../assets/imgs/home/initial-title.png";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#0f0c1a] via-[#1a1428] to-[#2b1d3a] text-white">
 
-      <section className="flex items-center justify-center"></section>
+      <section>
       <div
         className="relative h-[100vh] bg-cover bg-top flex items-center justify-center"
         style={{ backgroundImage: `url(${background})` }}
@@ -19,6 +19,7 @@ export default function Home() {
           className="relative z-10 h-60 md:h-80 drop-shadow-[0_0_40px_rgba(255,200,0,0.4)]"
         />
       </div>
+      </section>
 
       <section className="flex items-center justify-center">
         <div className="flex flex-col items-center gap-12 text-center max-w-2xl px-6">
@@ -30,13 +31,12 @@ export default function Home() {
           />
 
           <p className="text-gray-300 text-lg leading-relaxed">
-            Uma jornada épica começa aqui. Prepare-se para explorar reinos,
-            enfrentar criaturas lendárias e provar seu valor.
+            Este projeto é o resultado de uma jornada de aprendizado no mundo do game development. Desenvolvido como um projeto estudantil, meu objetivo foi aprender os fundamentos do desenvolvimento de jogos, praticar lógica e consolidar meus conhecimentos na linguagem C. Utilizando a biblioteca Raylib, criei um jogo totalmente em C, que explora mecânicas simples, mas com uma identidade visual única. Agora, o código está disponível no meu repositório no GitHub, convidando você a explorar, aprender e se inspirar nessa experiência.
           </p>
 
           <a className="mt-4 px-8 py-3 bg-[#c9a227] text-black font-semibold rounded-lg 
           hover:scale-105 transition-transform duration-300 shadow-lg mb-10" href="https://github.com/Jaca7x/Knight-s-Quest" target="_blank">
-            Começar Aventura
+            Começar Jornada!
           </a>
 
         </div>
