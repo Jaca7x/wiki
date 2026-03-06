f73347a004349a2e612e0a6f88dc38ca6ee0a744
Jaca <eduardo.costa.miguel@escola.pr.gov.br>
2026-03-05 21:34:52 -0300

    feat: Iniciando estrutura da page monster

---
+++ src/pages/Monsters.tsx
@@ -0,0 +1,11 @@
+export default function Monsters() {
+    return(
+    <div className="min-h-screen bg-gradient-to-b from-[#0f0c1a] via-[#1a1428] to-[#2b1d3a] text-white">
+        <section>
+            <div className="bg-black">
+                <p>dadad</p>
+            </div>
+        </section>
+    </div>
+    )
+}
