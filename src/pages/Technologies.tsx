9f1359ac83d8c9a2ed6954f2627a975bc01956b7
Jaca <eduardo.costa.miguel@escola.pr.gov.br>
2026-03-27 16:02:14 -0300

    feat: Adicionando componente de arquitetura para render

--- src/pages/Technologies.tsx
+++ src/pages/Technologies.tsx
@@ -1,10 +1,12 @@
 import StackCards from "@/features/technologies/components/StackCards"
+import ArchitectureExplorer from "@/features/technologies/components/ArchitectureExplorer"
 
 export default function Technologies() {
 
     return (
         <main>
             <StackCards />
+            <ArchitectureExplorer />
         </main>
     );
 }
