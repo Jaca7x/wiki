7ca87df1eeb915c345da5921e4798afb9e0d27b7
Jaca <eduardo.costa.miguel@escola.pr.gov.br>
2026-04-20 21:32:38 -0300

    style: estilizando focus da page

--- src/features/monster/components/MonsterCard.tsx
+++ src/features/monster/components/MonsterCard.tsx
@@ -31,13 +31,25 @@ export default function MonsterCard({
     marginLeft = 0,
     marginBottom = 0
 }: MonsterCardProps) {
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault();
+            onClick();
+        }
+    };
+
     return (
         <div
+            tabIndex={0}
+            role="button"
             className="flex flex-col relative items-center justify-between bg-black/40 p-6 rounded-lg 
                        w-80 min-h-80 shrink-0 snap-center
                        transition-all duration-300 border border-transparent 
                        hover:border-[#c9a227]/30 cursor-pointer 
-                       z-10 hover:z-20"
+                       z-10 hover:z-20 focus-visible:ring-2 focus-visible:ring-[#FACC15] focus-visible:border-[#FACC15]/5 focus-visible:ring-offset-8 focus-visible:ring-offset-[#FACC15] 
+                    focus:bg-[#FACC10]/5"
+            onKeyDown={handleKeyDown}
             onMouseEnter={onHover}
             onMouseLeave={onLeave}
             onClick={onClick}
